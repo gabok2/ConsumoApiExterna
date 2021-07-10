2580adf318a881742fc81cd5bcd11d0753ecdc89
gabriel <53623479+gabok2@users.noreply.github.com>
2021-07-10 01:11:24 -0300

    update
    update

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import express,{Request, Response,} from "express";
 
 const routes = express.Router();
 
- const teste = [];
+
 
 routes.post('/login/',async(req:Request,res:Response) => { 
   const {email, password} =req.body
@@ -11,39 +11,26 @@ routes.post('/login/',async(req:Request,res:Response) => {
       email:email,
       password:password
   });
-  const {token}=login.data;
-
-  teste.push(token);
+  
   res.send(login.data);
   
 
 });
 
-routes.get('/coffees/',async(request:Request,response:Response) => {
 
-  try{
+routes.get('/coffees/', async (request: Request, response: Response) => {
+  try {
     const { token } = request.headers;
 
-    const testetoken = teste.find((testetoken) => testetoken.token === token);
-  
-    //if (!testetoken) return response.status(400).json({ error: "Customer not found" });
-     const coofe =await axios.get('https://api.app.amachains.io/coffees',{
-       headers:{
-        Authorization: `Bearer ${JSON.stringify(testetoken)}`
-       }
-       
-     });
-  
-    
-  
-    response.json(coofe);
+    const { data } = await axios.get('https://api.app.amachains.io/coffees', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  }catch(error){
+    return response.json(data);
+  } catch (error) {
     console.log(error);
   }
-
 });
-
-console.log(teste);
-
 export {routes};
